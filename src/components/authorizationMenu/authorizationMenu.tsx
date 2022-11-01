d9d2abaa3d761092481fde16206f24a77f7a1516
kpovel <kar.pavel123@gmail.com>
2022-11-01 23:24:07 +0200

    Changed method of obtaining input value

--- src/components/authorizationMenu/authorizationMenu.tsx
+++ src/components/authorizationMenu/authorizationMenu.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable indent */
 import "./authorization.css";
 import {Link, useNavigate} from "react-router-dom";
-import {useState} from "react";
+import {FormEvent, useRef, useState} from "react";
 import {useCookies} from "react-cookie";
 import {add} from "date-fns";
 import eye from "../img/eye.svg";
@@ -12,16 +12,18 @@ export function AuthorizationMenu() {
     const [visibilityPassword, setVisibilityPassword] = useState(false);
     const [cookie, setCookie] = useCookies(["isAuthorized"]);
     const navigate = useNavigate();
+    const usernameRef = useRef<HTMLInputElement | null>(null);
+    const passwordRef = useRef<HTMLInputElement | null>(null);
 
     function showPassword() {
         setVisibilityPassword(!visibilityPassword);
     }
 
-    function authorisationUser(event: any) {
+    function authorisationUser(event: FormEvent) {
         const USERNAME = "kpovel";
         const PASSWORD = "1234";
-        const inputUsername = event.target.username.value;
-        const inputPassword = event.target.password.value;
+        const inputUsername = usernameRef.current?.value;
+        const inputPassword = passwordRef.current?.value;
         const isCorrectAuthorizationData = USERNAME === inputUsername && PASSWORD === inputPassword;
 
         if (isCorrectAuthorizationData) {
@@ -50,20 +52,24 @@ export function AuthorizationMenu() {
                 </div>
                 <form className="input-forms" onSubmit={authorisationUser}>
                     <p>
-                        <label className="label-login" htmlFor="username">
-                            Username:
-                        </label>
-                        <input className="form-textbox form-textbox__active" name="username" type="text"
-                               placeholder="ID"/>
+                        <label className="label-login" htmlFor="username">Username:</label>
+                        <input className="form-textbox form-textbox__active"
+                               name="username" type="text"
+                               placeholder="ID"
+                               ref={usernameRef}
+                        />
                     </p>
                     <p>
-                        <label className="label-login" htmlFor="password">
-                            Password:
-                        </label>
-                        <input className="form-textbox form-textbox__active" name="password"
+                        <label className="label-login" htmlFor="password">Password:</label>
+                        <input className="form-textbox form-textbox__active"
+                               name="password"
                                type={visibilityPassword ? "text" : "password"}
-                               placeholder="Password"/>
-                        <img src={visibilityPassword ? eye : eyeSlash} className="show-password"
+                               placeholder="Password"
+                               ref={passwordRef}
+                        />
+                        <img src={visibilityPassword ? eye : eyeSlash}
+                             className="show-password"
+                             alt={"show-password"}
                              onClick={showPassword}/>
                     </p>
                     <input type="submit" className="login-button" value="Log in"/>
