8ddcfa354688d480ab5678374bd5a6d199241141
Pavlo Karas <kar.pavel123@gmail.com>
2022-11-09 11:34:09 +0200

    Added the configs files

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "react/react-in-jsx-scope": 0,
+        "react/jsx-indent-props": 0,
+        "react/prop-types": 0,
+        "indent": [
+            "error",
+            4,
+            {"SwitchCase": 1},
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
