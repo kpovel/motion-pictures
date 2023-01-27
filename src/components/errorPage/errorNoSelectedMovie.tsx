9134cb442f454d33242ec9051013d61a000432a0
Pavlo Karas <kar.pavel123@gmail.com>
2023-01-27 20:37:23 +0200

    Styled error pages using MUI

--- src/components/errorPage/errorNoSelectedMovie.tsx
+++ src/components/errorPage/errorNoSelectedMovie.tsx
@@ -1,11 +1,21 @@
+import {Box, Typography} from "@mui/material";
+
 export default function ErrorNoSelectedMovie() {
     return (
-        <div className="error-page">
-            <h1>Oops!</h1>
+        <Box
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                width: "100%",
+            }}
+        >
+            <Typography component="h1" variant="h2">Oops!</Typography>
             <p>Sorry, an unexpected error has occurred. </p>
             <p>
                 <i> There aren&apos;t selected movies</i>
             </p>
-        </div>
+        </Box>
     );
 }
